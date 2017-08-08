62170b19ccbc2f15fcc6f5ea9c5bb1d101e5610c
Rakesh Jain <rakeshvora2007@gmail.com>
2017-08-08 18:42:27 -0500

    first commit

---
+++ Activity Counter Application/webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require('path')
+
+module.exports = {
+	entry: path.resolve(__dirname + "/src/index.js"),
+	output: {
+		path: path.resolve(__dirname + "/dist/assets"),
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: __dirname + '/dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+        use: {
+        loader: "babel-loader",
+				options: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			}
+    },
+		{
+			test: /\.json$/,
+			exclude: /(node_modules)/,
+			loader: "json-loader"
+		},
+		{
+			test: /\.css$/,
+			loader: "style-loader!css-loader!autoprefixer-loader"
+		},
+		{
+			test: /\.scss/,
+			loader: "style-loader!css-loader!autoprefixer-loader!sass-loader"
+		}
+		]
+	}
+}
